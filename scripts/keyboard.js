fbd7ef96c01f85f7e89f18594dd447b803518f25
SPHsofi <Sofia_pash@mail.ru>
2024-01-14 09:52:01 +0300

    feat: create module keyboard

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,25 +1,44 @@
-var alphabet = 'ABCDIFGHIJKLMNOPQRSTUVWXYZ';
-var keyboard = document.createElement('div');
-keyboard.className = 'keyboard';
-document.body.appendChild(keyboard);
+export class Keyboard {
+  constructor(alphabet) {
+    this.alphabet = alphabet;
+    this.inputField = document.createElement('input');
+    this.keyboard = document.createElement('div');
+    this.keyboard.className = 'letters';
+    this.initializeButtons();
+    document.body.appendChild(this.inputField);
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
 
-var inputField = document.createElement('input');
-document.body.appendChild(inputField);
-
-for (var i = 0; i < alphabet.length; i++) {
-    var button = document.createElement('button');
-    button.textContent = alphabet[i];
-    keyboard.appendChild(button);
-
-    button.addEventListener('click', function (event) {
-        inputField.value += event.target.textContent;
+  createButton(letter) {
+    const button = document.createElement('button');
+    button.textContent = letter;
+    this.keyboard.appendChild(button);
+    button.addEventListener('click', () => {
+      this.inputField.value += letter;
     });
+  }
+
+  initializeButtons() {
+    for (let i = 0; i < this.alphabet.length; i++) {
+      this.createButton(this.alphabet[i]);
+    }
+  }
+
+  handleKeyDown(event) {
+    const key = event.key.toUpperCase();
+    if (this.alphabet.includes(key)) {
+      this.inputField.value += key;
+    }
+  }
+
+  renderKeyboard() {
+    const section = document.createElement('section');
+    section.className = 'keyboard__section section';
+    section.append(this.keyboard);
+    return section;
+  }
 }
 
-document.addEventListener('keydown', function (event) {
-    var key = event.key.toUpperCase();
-    if (alphabet.includes(key)) {
-        inputField.value += key;
-    }
-});
-
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const myKeyboard = new Keyboard(alphabet);
+document.body.append(myKeyboard.renderKeyboard());
