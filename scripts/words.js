50dbbb822ba993acb3379a772f197ea9ba95d687
SPHsofi <Sofia_pash@mail.ru>
2024-01-14 09:50:32 +0300

    refactor: delete id from words array and change question lang

--- scripts/words.js
+++ scripts/words.js
@@ -1,53 +1,42 @@
 export const words = [
     {
-        id: "0",
         question: "A programming language for creating interactive web pages?",
         word: "JAVASCRIPT",
     },
     {
-        id: "1",
         question: "A standard for web page styles and layout?",
         word: "CSS",
     },
     {
-        id: "2",
         question: "A structured data format used to transfer information between the client and the server?",
         word: "JSON",
     },
     {
-        id: "3",
         question: "A tool for building, testing, and optimizing JavaScript code?",
         word: "WEBPACK",
     },
     {
-        id: "4",
         question: "The document markup language used to create the content of web pages?",
         word: "HTML",
     },
     {
-        id: "5",
-        question: "Инструмент для управления зависимостями в веб-проектах?",
+        question: "A tool for managing dependencies in web projects?",
         word: "NPM",
     },
     {
-        id: "6",
         question: "Software used to organize and manage databases?",
         word: "SQL",
     },
     {
-        id: "7",
         question: "A tool for code versioning and collaboration on projects?",
         word: "GIT",
     },
     {
-        id: "8",
         question: "A data storage technology based on a distributed network of nodes?",
         word: "BLOCKCHAIN",
     },
     {
-        id: "9",
         question: "A standard for encoding characters from various alphabets and writing systems?",
         word: "UNICODE",
     },
-
 ]
