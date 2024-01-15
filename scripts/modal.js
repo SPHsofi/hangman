cf169e33ec0cba19b395075c8b2782bfbf8af6f5
SPHsofi <Sofia_pash@mail.ru>
2024-01-15 16:31:14 +0300

    feat: add end modal method

--- scripts/modal.js
+++ scripts/modal.js
@@ -26,6 +26,24 @@ export class Modal {
     });
   }
 
+  renderEndModal(message, word) {
+    const button = document.createElement('button');
+    button.className = "restart__btn";
+    button.textContent = "Start again";
+    this.resetButton = button;
+
+    this.modal.innerHTML = `
+      <h2 class = "message-modal">${message}</h2>
+      <p class = "word-modal">${word}</p>
+    `;
+
+    this.modal.append(button);
+    this.modal.className = "modal-end";
+    this.modal.querySelector(".restart__btn").addEventListener("click", () => {
+      this.hideModal();
+    });
+  }
+
   // метод для вызова модального окна при выигрыше
   showWinModal(word) {
     this.renderModal("You've won!", word);
@@ -33,7 +51,11 @@ export class Modal {
 
   // метод для вызова модального окна при проигрыше
   showLoseModal(word) {
-    this.renderModal("You've lost", word );
+    this.renderModal("You've lost", word);
+  }
+  // метод для вывода окончания игры
+  showEndModal() {
+    this.renderEndModal("The game end", "Want restart?");
   }
 
   // Метод для скрытия модального окна
